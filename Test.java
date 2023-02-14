3fb6e5c930657dbb61141ecb3f9333199b771fea
Alpmant <99615242+Alpmant@users.noreply.github.com>
2023-02-15 01:34:04 +0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,47 @@
+import java.io.Console;
+import java.util.Scanner;
+
+
+
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+              for(int i=2;i<6;i++){
+                  if(i==4)
+                      continue;
+                  System.out.println(i);
+              }
+              for(int k=6;k<100;k++){
+                  if(k%2!=0&&k%3!=0&&k%5!=0){
+                      System.out.println(k);
+                  }
+              }
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
